ea39249c3c84864be5c9d13cf28ad77f32fbfb00
Denis Krivostchokov <densomart@gmail.com>
2013-05-16 19:46:12 +0700

    debuging 2 ( change @source_field to @@source_field)

--- lib/preferences_boxer/store/db_store.rb
+++ lib/preferences_boxer/store/db_store.rb
@@ -3,31 +3,28 @@ module PreferencesBoxer
     class << self
       def configure block
           block.call self
-          # eval "const_get( @source_record.class.name ).class_eval { store @source_field }"
-          # eval "p '!!'; p const_get( @source_record.class.name ).all"
-          # p const_get( @source_record.class )
-          # .send store :url; end }
-          User.class_eval { store :settings } #включает сериализацию
-          @source_record.reload # reload for update, after store method call
+          @@source_record.class.class_eval { store @@source_field }
+          # User.class_eval { store :settings } #включает сериализацию
+          @@source_record.reload # reload for update, after store method call
       end
 
       def source_record=(rec)
-        @source_record = rec
+        @@source_record = rec
       end
 
       def source_field=(field)
-        @source_field = field
+        @@source_field = field
       end
 
       def set(name, val)
-        rec = @source_record
-        eval "rec.#{@source_field}.unserialize[name.to_sym] = val"
+        rec = @@source_record
+        eval "rec.#{@@source_field}.unserialize[name.to_sym] = val"
         rec.save
       end
 
       def get(name)
         begin # rescue, if value == nil
-          eval "@source_record.#{@source_field}.unserialize[name.to_sym]"
+          eval "@@source_record.#{@@source_field}.unserialize[name.to_sym]"
         rescue
         end
       end
